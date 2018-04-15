06dfe09427360e9d819109e8c89e5d638588eea8
Samuel Sharp <samuelsharp4@gmail.com>
2018-04-14 20:30:48 -0700

    Removed HappyPet Instructions

--- src/HappyPet.java
+++ src/HappyPet.java
@@ -1,15 +1,9 @@
 import javax.swing.JOptionPane;
 
 public class HappyPet {
-	
-	// 2. Add the following variable to the next line: static int happinessLevel = 0;
 	static int happinessLevel = 0;
-	// this will be used to store the happiness of your pet
-	
 	public static void main(String[] args) {
-		// 1. Ask the user what kind of pet they want to buy, and store their answer in a variable
 		String Pet = JOptionPane.showInputDialog(null, "What pet do you want to buy?");
-		// 7. REPEAT steps 3 - 6 enough times to make your pet happy!
 		for(int timeGiven = 0; timeGiven < 50; timeGiven++) {
 			int task = JOptionPane.showOptionDialog(null, "What do you want to do to your pet?", "Pet Option", 0, JOptionPane.INFORMATION_MESSAGE, null,
 					new String[] { "Cuddle", "Food", "Water" }, null);
