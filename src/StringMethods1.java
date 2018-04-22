283c133f6db09692b65ea08cb25550c45870559a
NickSanford4 <league@WTS-IM-6.attlocal.net>
2018-04-21 17:08:32 -0700

    StringMethods1 (IP), Good Kid MP3 removed

--- src/StringMethods1.java
+++ src/StringMethods1.java
@@ -5,18 +5,14 @@
 
 public class StringMethods1 {
 	public static void main(String[] args) {
-		
-		// 1. Create a String variable and initialize it to whatever you want
+		String TheKeyToLife = "The Key To Life Is To Not Kill Anyone.";
 
-		// 2. Print the 3rd char of your String to the console.
-		//    HINT: .charAt
-		
-		// 3. Print the length of your String to the console.
-		//    HINT: .length
-
+		System.out.println(TheKeyToLife.length());
 		// 4. Using a for loop, print one char at time to the console.
 		//    HINT: use .length() to determine how many loops
-
+		for (int times = 0; times<TheKeyToLife.length();times++) {
+			System.out.println(TheKeyToLife.charAt(times));
+		}
 		
 		// 5. Pick a char inside your String, and use a loop to determine
 		//    what position/index in the String the char is located.
